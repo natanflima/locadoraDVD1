4d147b6295c1ac5ba73b4c3d1dff8cd7b04656fd
Natan Lima <88110272+natanflima@users.noreply.github.com>
2023-04-12 21:10:46 -0300

    Adicionando Classe Faixa Etária

---
+++ src/FaixaEtaria.java
@@ -0,0 +1,26 @@
+
+public class FaixaEtaria {
+   private int de;
+   private int ate;
+
+   public FaixaEtaria(int de, int ate) {
+      this.de = de;
+      this.ate = ate;
+   }
+
+   public int getDe() {
+      return de;
+   }
+
+   public void setDe(int de) {
+      this.de = de;
+   }
+
+   public int getAte() {
+      return ate;
+   }
+
+   public void setAte(int ate) {
+      this.ate = ate;
+   }
+}
